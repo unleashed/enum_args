81121d738f7829ab9e6b2c590b98606d618b1263
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-06-17 17:02:26 +0200

    spec/enum_args: change class methods to use the "enum_args_" prefix

--- spec/enum_args_spec.rb
+++ spec/enum_args_spec.rb
@@ -7,30 +7,31 @@ RSpec.describe EnumArgs do
 
   context 'when including EnumArgs' do
     subject { Helpers::IncludeEnumArgs.new }
+    let(:accessor_method) { subject.class.send(:enum_args_accessor_method) }
+    let(:enum_args) { subject.send(accessor_method) }
 
     it { is_expected.to be_a(Enumerable) }
     it { is_expected.to be_a(EnumArgs::ProxiedEnumerable) }
-    it { is_expected.to respond_to(subject.class.enum_accessor_method) }
+    it { is_expected.to respond_to(subject.class.enum_args_accessor_method) }
 
-    it_behaves_like 'an enumerable', Helpers::IncludeEnumArgs.enum_accessor_method
-    it_behaves_like 'an enumerator', Helpers::IncludeEnumArgs.enum_accessor_method
+    it_behaves_like 'an enumerable', Helpers::IncludeEnumArgs.enum_args_accessor_method
+    it_behaves_like 'an enumerator', Helpers::IncludeEnumArgs.enum_args_accessor_method
 
     it_expects_to 'raise on invocation errors'
 
     context 'when changing the default dynamic parameters' do
-      let(:enum) { subject.send(subject.class.send(:enum_accessor_method)) }
       let(:new_params) { { config: 'different', resumeinfo: 'none' } }
 
       before do
-        enum.args = rand.to_s
-        enum.using = new_params
+        enum_args.args = rand.to_s
+        enum_args.using = new_params
       end
 
       it 'receives the changed default parameters' do
-        args = Array(enum.args).dup
+        args = Array(enum_args.args).dup
         args << new_params
 
-        expect(subject).to receive(enum.method_name).with(*args)
+        expect(subject).to receive(enum_args.method_name).with(*args)
 
         subject.each.next rescue StopIteration
       end
