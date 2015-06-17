174740d1861862b87db274f5afe4d43896454a82
Alejandro Martinez Ruiz <alex@flawedcode.org>
2015-06-17 15:55:20 +0200

    spec/enum_args: add explicit testing of changing the default parameters

--- spec/enum_args_spec.rb
+++ spec/enum_args_spec.rb
@@ -16,6 +16,26 @@ RSpec.describe EnumArgs do
     it_behaves_like 'an enumerator', Helpers::IncludeEnumArgs.enum_accessor_method
 
     it_expects_to 'raise on invocation errors'
+
+    context 'when changing the default dynamic parameters' do
+      let(:enum) { subject.send(subject.class.send(:enum_accessor_method)) }
+      let(:new_params) { { config: 'different', resumeinfo: 'none' } }
+
+      before do
+        enum.args = rand.to_s
+        enum.using = new_params
+      end
+
+      it 'receives the changed default parameters' do
+        args = Array(enum.args).dup
+        args << new_params
+
+        expect(subject).to receive(enum.method_name).with(*args)
+
+        subject.each.next rescue StopIteration
+      end
+    end
+
   end
 
   context 'when using EnumArgs::Proxy directly' do
